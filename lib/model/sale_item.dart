a4505c8b3c59f974996e99220cababd37f716ca9
nanirocks125 <nanirocks125@gmail.com>
2026-03-30 16:40:16 +0530

    small change

--- lib/model/sale_item.dart
+++ lib/model/sale_item.dart
@@ -2,7 +2,7 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'sale_item.g.dart';
 
-@JsonSerializable(explicitToJson: true, fieldRename: FieldRename.snake)
+@JsonSerializable(explicitToJson: true)
 class SaleItem {
   // Keeping 'cat_id' for backwards compatibility with your existing data
   @JsonKey(name: 'cat_id')
